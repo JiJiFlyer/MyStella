32e0ffa463e12ec1a647e33831b9527aa06e76a6
JiJiFlyer <JiJiFlyer@users.noreply.github.com>
2018-01-31 23:06:06 +0800

    费率（完成左一）

---
+++ echart_fee.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="echart_fee.aspx.vb" Inherits="Jamada_4_0.echart_fee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ECharts">
+    <title>echart_fee</title>
+    <script src="js/common.js"></script>
+    <script src="echars/echarts.js" charset='utf-8'></script>
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script src="echars/macarons.js"></script>
+    <script src="../js/RPC.js"></script>    
+    <script src="echars/codemirror.js"></script>
+    <script src="echars/javascript.js"></script>
+</head>
+<body>
+    <%=errorshow1%><br />
+    <%=errorshow2%>
+    <div id="fee6601" style="width: 800px; height: 600px;float:left;"></div>
+    <div id="fee6602" style="width: 800px; height: 600px;float:left;"></div>
+    <div id="fee6603" style="width: 800px; height: 600px;float:left;"></div>
+    <div id="feeQJ" style="width: 800px; height: 600px;float:left;"></div>
+    <div id="feeML" style="width: 800px; height: 600px;float:left;"></div>
+    
+    <script type="text/javascript">
+        var echart_fee6601 = echarts.init(document.getElementById('fee6601'), 'macarons');
+            option6601 = {
+                title: {
+                    text: '销售费用率',
+                    subtext: '年度数据',
+                },
+                tooltip: {
+                    trigger: 'item',
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        mark: { show: true },
+                        restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                calculable: true,
+                legend: {
+                    data: ['费用', '费率']
+                },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: <%=flDate%>
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    name: '费用',
+                },
+                {
+                    type: 'value',
+                    name: '费率',
+                    axisLabel: {
+                        formatter: '{value}'
+                    }
+                }
+            ],
+            series: [
+                {
+                    name: '费用',
+                    type: 'bar',
+                    data: <%=Bdata%>
+                },
+                {
+                    name: '费率',
+                    type: 'line',
+                    lineStyle: { width: 20 },
+                    symbolSize: 7,
+                    yAxisIndex: 1,
+                    data: <%=Ldata%>,
+                    markPoint: { data:[<%=Lmark%>]}
+                } 
+            ]
+        };
+
+            echart_fee6601.setOption(option6601);
+    </script>
+</body>
+</html>
---
+++ echart_fee.aspx.vb
@@ -0,0 +1,81 @@
+﻿Public Class echart_fee
+    Inherits System.Web.UI.Page
+
+    Public errorshow1 As String
+    Public errorshow2 As String
+    Public flDate As New StringBuilder()
+    Public Bdata As New StringBuilder()
+    Public Ldata As New StringBuilder()
+    Public Lmark As New StringBuilder()
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim db As DB
+        Dim drDB As SqlClient.SqlDataReader
+        Dim sSql As String
+        db = New DB
+        Try
+            flDate.Append("[")
+            Dim i As Integer
+            Dim fee_6601(13) As Double
+            Dim fee_6001(13) As Double
+            Dim fee_6051(13) As Double
+            Dim month As Integer = Format(Now(), "MM")
+            For i = 1 To month
+                flDate.Append("""" & i.ToString() & "月"",")
+            Next i
+            flDate.Append("]")
+
+            '6601 每次循环读一次sql写入Bdata,存fee数组用于计算费率
+            '销售费用
+            Bdata.Append("[")
+            For i = 1 To month
+                sSql = "select isnull(sum(jf_bala),0) as xsfy from f_voucher_entry a inner join f_voucher b on b.autoinc=a.voucherid 
+                        where itemno like '6601%' and SUBSTRING(b.billdate,5,2)= " & i.ToString() & " and fyear=DateName(YEAR,GetDate())"
+                drDB = db.GetDataReader(sSql)
+                drDB.Read()
+                fee_6601(i) = drDB.Item("xsfy")
+                Bdata.Append("" & Format(drDB.Item("xsfy"), "#.##") & ",")
+                drDB.Close()
+            Next i
+            Bdata.Append("]")
+
+            '以月份作控制变量循环 存fee数组用于计算费率
+            '6001
+            For i = 1 To month
+                sSql = "select isnull(sum(df_bala),0) as fee from f_voucher_entry a inner join f_voucher b on b.autoinc=a.voucherid 
+                        where itemno like '6001%' and SUBSTRING(b.billdate,5,2)= " & i.ToString() & " and fyear=DateName(YEAR,GetDate())"
+                drDB = db.GetDataReader(sSql)
+                drDB.Read()
+                fee_6001(i) = drDB.Item("fee")
+                drDB.Close()
+            Next i
+
+            '6051
+            For i = 1 To month
+                sSql = "select isnull(sum(df_bala),0) as fee from f_voucher_entry a inner join f_voucher b on b.autoinc=a.voucherid 
+                        where itemno like '6051%' and SUBSTRING(b.billdate,5,2)= " & i.ToString() & " and fyear=DateName(YEAR,GetDate())"
+                drDB = db.GetDataReader(sSql)
+                drDB.Read()
+                fee_6051(i) = drDB.Item("fee")
+                drDB.Close()
+            Next i
+
+
+            '销售费率
+            Dim xsfl As Double
+            Ldata.Append("[")
+            For i = 1 To month
+                xsfl = fee_6601(i) / (fee_6001(i) + fee_6051(i))
+                Ldata.Append("" & Format(xsfl, "#.##") & ",")
+                Lmark.Append("{name : '销售费用率',value: " & Format(xsfl, "#.##") & ", xAxis: " & i - 1 & ", yAxis: " & Format(xsfl, "#.##") & "},")
+            Next i
+            Ldata.Append("]")
+
+        Catch ex As Exception
+            DB.Close(db, drDB)
+            errorshow1 = ex.Message
+            errorshow2 = ex.StackTrace
+        End Try
+    End Sub
+
+End Class
