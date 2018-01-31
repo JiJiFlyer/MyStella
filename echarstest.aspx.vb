845cc1889bc05a32f7ea6ec2698b5654996665b5
JiJiFlyer <JiJiFlyer@users.noreply.github.com>
2018-01-31 20:46:46 +0800

    读取数据库区间设置，计算相应数据并呈现
    支持最少1个节点（2区间），最多10个节点（11区间）的账龄区间自定义。
    用户修改数据库界面待处理。

--- echarstest.aspx.vb
+++ echarstest.aspx.vb
@@ -42,7 +42,7 @@
         Dim sSql As String
         Try
             '读取数据库中区间设置
-            '定义agenumber用于得到区间个数
+            '定义agenumber用于得到区间节点数（区间数-1），用户最多设置十个节点
             Dim agenumber As Integer = 0
             '定义AgeData数组用于存数据库中读取的区间设置
             Dim AgeData(0 To 9) As Integer
@@ -57,20 +57,18 @@
                 agenumber = agenumber + 1
             End While
             drDB.Close()
-            '标签行呈现
+            '标签行呈现,agenumber为0时要再考虑
             AgeLabel.Append("<th>1-" & AgeData(0).ToString() & "天</th>")
             For i = 1 To agenumber - 1
                 AgeLabel.Append("<th>" & (AgeData(i - 1) + 1).ToString() & "-" & AgeData(i).ToString() & "天</th>")
             Next i
             AgeLabel.Append("<th>" & AgeData(agenumber - 1).ToString() & "天以上</th>")
 
-            '账龄区间总额定义，用于累加该区间内金额，日后需要能自定义
-            Dim a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 As Double
-            a1 = 0
-            a2 = 0
-            a3 = 0
-            a4 = 0
-            a5 = 0
+            '账龄区间总额数组定义，用于累加该区间内金额
+            Dim agetotal(10) As Double
+            For i = 0 To 10
+                agetotal(i) = 0
+            Next i
             '该客户之前a_bal的累加和
             Dim pre_t_bal As Double = 0
             '该客户当前凭证经修正的a_bal
@@ -89,18 +87,16 @@
             drDB.Close()
             '第二个查询：先判断是否是同一客户
             '再判断a_bal是否是超过t_bal并做调整
-            '最后判断经过调整的a_bal处于哪一账龄区间，加至相应的a
+            '最后判断经过调整的a_bal处于哪一账龄区间，加至相应的agetotal
             sSql = "select * from dbo.echart_accountage order by clientname,billdate desc"
             drDB = db.GetDataReader(sSql)
             While drDB.Read
                 '判断是否为新客户
                 If drDB.Item("clientname") <> pre_clientname Then
-                    '若是，重置前一客户统计的pre_t_bal和a
-                    a1 = 0
-                    a2 = 0
-                    a3 = 0
-                    a4 = 0
-                    a5 = 0
+                    '若是，重置前一客户统计的pre_t_bal和agetotal
+                    For i = 0 To 10
+                        agetotal(i) = 0
+                    Next i
                     pre_t_bal = 0
                     '更新上一客户的数据
                     AccountAge.Append(StringHolder1)
@@ -118,20 +114,179 @@
                 End If
                 '始终更新前三列,存入用于寄存的string变量
                 StringHolder1 = """<tr><td>" & drDB.Item("clientname") & "</td><td>" & drDB.Item("bal_fx") & "</td><td>" & drDB.Item("t_bal") & "</td>"""
-                '这个判断用于统计不同区间各自的总金额，区间日后需要自定义，可先获得用户设置的区间个数，新增变量控制分支
-                If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= 30 Then
-                    a1 = a1 + a_bal
-                ElseIf drDB.Item("a_age") >= 31 And drDB.Item("a_age") <= 60 Then
-                    a2 = a2 + a_bal
-                ElseIf drDB.Item("a_age") >= 61 And drDB.Item("a_age") <= 90 Then
-                    a3 = a3 + a_bal
-                ElseIf drDB.Item("a_age") >= 91 And drDB.Item("a_age") <= 120 Then
-                    a4 = a4 + a_bal
-                ElseIf drDB.Item("a_age") >= 121 Then
-                    a5 = a5 + a_bal
-                End If
-                '更新账龄数据,存入用于寄存的string变量
-                StringHolder2 = """<td>" & a1 & "</td><td>" & a2 & "</td><td>" & a3 & "</td><td>" & a4 & "</td><td>" & a5 & "</td></tr>"""
+                '这个判断用于统计不同区间各自的总金额。先select用户设置的区间个数，根据区间的个数进行分支（简单粗暴）
+                Select Case agenumber
+                    Case 1
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td></tr>"""
+                    Case 2
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td></tr>"""
+                    Case 3
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td></tr>"""
+                    Case 4
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td></tr>"""
+                    Case 5
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td></tr>"""
+                    Case 6
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 And drDB.Item("a_age") <= AgeData(5) Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(5) + 1 Then
+                            agetotal(6) = agetotal(6) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td><td>" & agetotal(6) & "</td></tr>"""
+                    Case 7
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 And drDB.Item("a_age") <= AgeData(5) Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(5) + 1 And drDB.Item("a_age") <= AgeData(6) Then
+                            agetotal(6) = agetotal(6) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(6) + 1 Then
+                            agetotal(7) = agetotal(7) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td><td>" & agetotal(6) & "</td><td>" & agetotal(7) & "</td></tr>"""
+                    Case 8
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 And drDB.Item("a_age") <= AgeData(5) Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(5) + 1 And drDB.Item("a_age") <= AgeData(6) Then
+                            agetotal(6) = agetotal(6) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(6) + 1 And drDB.Item("a_age") <= AgeData(7) Then
+                            agetotal(7) = agetotal(7) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(7) + 1 Then
+                            agetotal(8) = agetotal(8) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td><td>" & agetotal(6) & "</td><td>" & agetotal(7) & "</td><td>" & agetotal(8) & "</td></tr>"""
+                    Case 9
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 And drDB.Item("a_age") <= AgeData(5) Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(5) + 1 And drDB.Item("a_age") <= AgeData(6) Then
+                            agetotal(6) = agetotal(6) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(6) + 1 And drDB.Item("a_age") <= AgeData(7) Then
+                            agetotal(7) = agetotal(7) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(7) + 1 And drDB.Item("a_age") <= AgeData(8) Then
+                            agetotal(8) = agetotal(8) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(8) + 1 Then
+                            agetotal(9) = agetotal(9) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td><td>" & agetotal(6) & "</td><td>" & agetotal(7) & "</td><td>" & agetotal(8) & "</td><td>" & agetotal(9) & "</td></tr>"""
+                    Case 10
+                        If drDB.Item("a_age") >= 1 And drDB.Item("a_age") <= AgeData(0) Then
+                            agetotal(0) = agetotal(0) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(0) + 1 And drDB.Item("a_age") <= AgeData(1) Then
+                            agetotal(1) = agetotal(1) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(1) + 1 And drDB.Item("a_age") <= AgeData(2) Then
+                            agetotal(2) = agetotal(2) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(2) + 1 And drDB.Item("a_age") <= AgeData(3) Then
+                            agetotal(3) = agetotal(3) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(3) + 1 And drDB.Item("a_age") <= AgeData(4) Then
+                            agetotal(4) = agetotal(4) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(4) + 1 And drDB.Item("a_age") <= AgeData(5) Then
+                            agetotal(5) = agetotal(5) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(5) + 1 And drDB.Item("a_age") <= AgeData(6) Then
+                            agetotal(6) = agetotal(6) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(6) + 1 And drDB.Item("a_age") <= AgeData(7) Then
+                            agetotal(7) = agetotal(7) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(7) + 1 And drDB.Item("a_age") <= AgeData(8) Then
+                            agetotal(8) = agetotal(8) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(8) + 1 And drDB.Item("a_age") <= AgeData(9) Then
+                            agetotal(9) = agetotal(9) + a_bal
+                        ElseIf drDB.Item("a_age") >= AgeData(9) + 1 Then
+                            agetotal(10) = agetotal(10) + a_bal
+                        End If
+                        '更新账龄数据,存入用于寄存的string变量
+                        StringHolder2 = """<td>" & agetotal(0) & "</td><td>" & agetotal(1) & "</td><td>" & agetotal(2) & "</td><td>" & agetotal(3) & "</td><td>" & agetotal(4) & "</td><td>" & agetotal(5) & "</td><td>" & agetotal(6) & "</td><td>" & agetotal(7) & "</td><td>" & agetotal(8) & "</td><td>" & agetotal(9) & "</td><td>" & agetotal(10) & "</td></tr>"""
+                End Select
                 '更新pre_clientname
                 pre_clientname = drDB.Item("clientname")
             End While
