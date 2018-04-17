1a138a641740908fbd6005f3fba434efe8995b96
JiJiFlyer <JiJiFlyer@users.noreply.github.com>
2018-04-17 20:00:27 +0800

    资产负债率表+资产构成

---
+++ echart_asset-liability.aspx
@@ -0,0 +1,189 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="echart_asset-liability.aspx.vb" Inherits="Jamada_4_0.echart_asset_liability" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>echart_asset-liability</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ECharts">
+    <script src="js/common.js"></script>
+    <script src="echars/echarts.js" charset='utf-8'></script>
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script src="echars/dark.js"></script>
+    <script src="echars/fee_tax.js"></script>
+    <script src="echars/vintage.js"></script>
+    <script src="../js/RPC.js"></script>
+    <script src="echars/codemirror.js"></script>
+    <script src="echars/javascript.js"></script>
+    <style type="text/css">
+        html, body {
+            margin: 0;
+            padding: 0 5px;
+            height: 100%;
+        }
+
+        .main {
+            background-color: black;
+            height: 100%;
+        }
+
+        .content {
+            height: 100%;
+            width: 50%;
+            float: left;
+            padding: 20px 0px;
+            background-color: #2b3642 /*#ebe8dd*/;
+        }
+    </style>
+</head>
+<body>
+    <%=errorshow1%><br />
+    <%=errorshow2%>
+    <div class="main">
+        <div id="AssetLiabilityBar" class="content"></div>
+        <div id="AssetPie" class="content"></div>
+    </div>
+    <script type="text/javascript">
+        var AssetLiabilityBar = echarts.init(document.getElementById('AssetLiabilityBar'), 'dark');
+        option_AssetLiabilityBar = {
+            grid: {
+                left: '8%',
+                right: '8%',
+            },
+            title: {
+                text: '资产负债率',
+                x: 'center'
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter: "{b0}月{a0}：{c0}万元<br/>{b1}月{a1}：{c1}%"
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: { show: true },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
+            calculable: true,
+            legend: {
+                data: ['资产', '资产负债率'],
+                top: '8%'
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: [<%=flDate%>]
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    name: '资产（万元）',
+                },
+                {
+                    type: 'value',
+                    name: '资产负债率',
+                    axisLabel: {
+                        formatter: '{value}%'
+                    }
+                }
+            ],
+            series: [
+                {
+                    name: '资产',
+                    type: 'bar',
+                    data: [<%=Bdata%>]
+                },
+                {
+                    name: '资产负债率',
+                    type: 'line',
+                    yAxisIndex: 1,
+                    data: [<%=Ldata%>],
+                    markPoint: {
+                        symbol: 'circle',
+                        symbolSize: 25,
+                        data: [<%=Lmark%>]
+                    }
+                }
+            ]
+        };
+
+        AssetLiabilityBar.setOption(option_AssetLiabilityBar);
+        window.addEventListener("resize", function () {
+
+            AssetLiabilityBar.resize();
+
+        });
+    </script>
+    <%--资产负债率柱折图--%>
+
+    <script type="text/javascript">
+        var myChart_AssetPie = echarts.init(document.getElementById('AssetPie'), 'dark');
+        option_AssetPie = {
+            title: {
+                text: '资产构成',
+                subtext:'企业当前资产构成情况',
+                x: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c}万元 ({d}%)"
+            },
+            legend: {
+                type: 'scroll',
+                top: '15%',
+                data: ['速动资产','非速动资产','非流动资产']
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: { show: true },
+                    dataView: { show: true, readOnly: false },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
+            calculable: true,
+            series: [
+                {
+                    name: '资产构成',
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '55%'],
+                    data: [<%=PData%>],
+                    label: {
+                        normal: {//数值显示设置
+                            formatter: '{b}:{d}%',
+                            textStyle: {
+                                fontWeight: 'normal',
+                                fontSize: 15
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+
+        myChart_AssetPie.setOption(option_AssetPie);
+        window.addEventListener("resize", function () {
+
+            myChart_AssetPie.resize();
+
+        });
+    </script>
+    <%--资产构成饼图--%>
+</body>
+</html>
---
+++ echart_asset-liability.aspx.vb
@@ -0,0 +1,174 @@
+﻿Public Class echart_asset_liability
+    Inherits System.Web.UI.Page
+    Public errorshow1 As String
+    Public errorshow2 As String
+    Public flDate As New StringBuilder()
+    Public Bdata As New StringBuilder()
+    Public Ldata As New StringBuilder()
+    Public Lmark As New StringBuilder()
+    Public PData As New StringBuilder()
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim db As DB
+        Dim drDB As SqlClient.SqlDataReader
+        Dim sSql As String
+        db = New DB
+        Try
+            Dim i As Integer
+            Dim month As Integer = Format(Now(), "MM")
+            For i = 1 To month
+                flDate.Append("" & i.ToString() & ",")
+            Next i
+            '资产额获取：年初余额+本年发生至某月累计
+            Dim asset_initial As Double
+            Dim liability_initial As Double
+            Dim asset(13) As Double
+            Dim liability(13) As Double
+            Dim asset_total As Double = 0
+            Dim liability_total As Double = 0
+            Dim zc As Double '资产
+            Dim fz As Double '负债
+            Dim zcfzl As Double '资产负债率
+
+            '资产期初余额
+            sSql = "select
+                    (select isnull(sum(inital_bala),0) jf from f_inital_balance a left join f_item b on a.itemno=b.itemno 
+                    where itemtype='4C569B56-F342-4F28-BB04-58797A1EC247' and fx_name='借方'and bookno=001)-
+                    (select isnull(sum(inital_bala),0) df from f_inital_balance a left join f_item b on a.itemno=b.itemno 
+                    where itemtype='4C569B56-F342-4F28-BB04-58797A1EC247' and fx_name='贷方'and bookno=001) asset_initial"
+            drDB = db.GetDataReader(sSql)
+            drDB.Read()
+            asset_initial = drDB.Item("asset_initial")
+            drDB.Close()
+
+            '负债期初余额
+            sSql = "select
+                    (select isnull(sum(inital_bala),0) df from f_inital_balance a left join f_item b on a.itemno=b.itemno 
+                    where itemtype='3A55DC83-38A7-4AF3-9672-9E79337563E9' and fx_name='贷方'and bookno=001)-
+                    (select isnull(sum(inital_bala),0) jf from f_inital_balance a left join f_item b on a.itemno=b.itemno 
+                    where itemtype='3A55DC83-38A7-4AF3-9672-9E79337563E9' and fx_name='借方'and bookno=001) liability_initial"
+            drDB = db.GetDataReader(sSql)
+            drDB.Read()
+            liability_initial = drDB.Item("liability_initial")
+            drDB.Close()
+
+            For i = 1 To month
+                '本年发生至某月累计
+                sSql = "select isnull(sum(jf_bala-df_bala),0) bala from f_voucher_entry a 
+                        inner join f_voucher b on a.voucherid=b.autoinc 
+                        left join f_item c on a.itemno=c.itemno
+                        where b.bookno=001 and itemtype='4C569B56-F342-4F28-BB04-58797A1EC247' and SUBSTRING(b.billdate,5,2)=  " & i.ToString() & " and fyear=DateName(YEAR,GetDate())"
+                drDB = db.GetDataReader(sSql)
+                drDB.Read()
+                asset(i) = drDB.Item("bala")
+                drDB.Close()    '资产每月发生额读取
+
+                sSql = "select isnull(sum(df_bala-jf_bala),0) bala from f_voucher_entry a 
+                        inner join f_voucher b on a.voucherid=b.autoinc 
+                        left join f_item c on a.itemno=c.itemno
+                        where b.bookno=001 and itemtype='3A55DC83-38A7-4AF3-9672-9E79337563E9' and SUBSTRING(b.billdate,5,2)= " & i.ToString() & "  and fyear=DateName(YEAR,GetDate())"
+                drDB = db.GetDataReader(sSql)
+                drDB.Read()
+                liability(i) = drDB.Item("bala")
+                drDB.Close()    '负债每月发生额读取
+
+                asset_total = asset_total + asset(i)
+                liability_total = liability_total + liability(i)
+                zc = asset_total + asset_initial    '资产类+ 资产期初余额
+                fz = liability_total + liability_initial    '负债类+ 资产期初余额
+                zcfzl = zc / fz * 100
+
+                Bdata.Append("" & Format(zc / 10000, "#.##") & ",") '换算单位为万元，保留两位小数
+                Ldata.Append("" & Format(zcfzl, "#.##") & ",")
+                Lmark.Append("{value: " & Format(zcfzl, "#.##") & ", xAxis: " & i - 1 & ", yAxis: " & Format(zcfzl, "#.##") & "},")
+            Next i
+
+            '速动资产填入
+            sSql = "select
+                        (select isnull(sum(inital_bala),0) jf 
+                        from f_inital_balance a 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                        where ib.cnname='速动资产' and fx_name='借方'and bookno=001)
+                        -
+                        (select isnull(sum(inital_bala),0) df 
+                        from f_inital_balance a 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                        where ib.cnname='速动资产' and fx_name='贷方'and bookno=001)
+                        +
+                        (select sum(jf_bala-df_bala)
+                        from f_voucher_entry a
+                        inner join f_voucher b on a.voucherid=b.autoinc 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc
+                        where ib.cnname='速动资产' and b.bookno=001 and fyear=DateName(YEAR,GetDate()))sudong"
+            drDB = db.GetDataReader(sSql)
+            drDB.Read()
+            PData.Append("{""value"":")
+            PData.Append("""" & Format(drDB.Item("sudong") / 10000, "#.##") & """,") '换算单位为万元，保留两位小数
+            PData.Append("""name"":""速动资产""},")
+            drDB.Close()
+
+            '非速动资产填入
+            sSql = "select
+                        (select isnull(sum(inital_bala),0) jf 
+                        from f_inital_balance a 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                        where ib.cnname='流动资产' and fx_name='借方'and bookno=001)
+                        -
+                        (select isnull(sum(inital_bala),0) df 
+                        from f_inital_balance a 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                        where ib.cnname='流动资产' and fx_name='贷方'and bookno=001)
+                        +
+                        (select sum(jf_bala-df_bala)
+                        from f_voucher_entry a
+                        inner join f_voucher b on a.voucherid=b.autoinc 
+                        left join f_item i on a.itemno=i.itemno
+                        left join f_item_subtype ib on i.itemsubtype=ib.autoinc
+                        where ib.cnname='流动资产' and b.bookno=001 and fyear=DateName(YEAR,GetDate()))feisudong"
+            drDB = db.GetDataReader(sSql)
+            drDB.Read()
+            PData.Append("{""value"":")
+            PData.Append("""" & Format(drDB.Item("feisudong") / 10000, "#.##") & """,") '换算单位为万元，保留两位小数
+            PData.Append("""name"":""非速动资产""},")
+            drDB.Close()
+
+            '非流动资产填入
+            sSql = "select
+                (select isnull(sum(inital_bala),0) jf 
+                from f_inital_balance a 
+                left join f_item i on a.itemno=i.itemno
+                left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                where ib.cnname='非流动资产' and fx_name='借方'and bookno=001)
+                -
+                (select isnull(sum(inital_bala),0) df 
+                from f_inital_balance a 
+                left join f_item i on a.itemno=i.itemno
+                left join f_item_subtype ib on i.itemsubtype=ib.autoinc 
+                where ib.cnname='非流动资产' and fx_name='贷方'and bookno=001)
+                +
+                (select sum(jf_bala-df_bala)
+                from f_voucher_entry a
+                inner join f_voucher b on a.voucherid=b.autoinc 
+                left join f_item i on a.itemno=i.itemno
+                left join f_item_subtype ib on i.itemsubtype=ib.autoinc
+                where ib.cnname='非流动资产' and b.bookno=001 and fyear=DateName(YEAR,GetDate()))feiliudong"
+            drDB = db.GetDataReader(sSql)
+            drDB.Read()
+            PData.Append("{""value"":")
+            PData.Append("""" & Format(drDB.Item("feiliudong") / 10000, "#.##") & """,") '换算单位为万元，保留两位小数
+            PData.Append("""name"":""非流动资产""},")
+            drDB.Close()
+
+        Catch ex As Exception
+            DB.Close(db, drDB)
+            errorshow1 = ex.Message
+            errorshow2 = ex.StackTrace
+        End Try
+    End Sub
+
+End Class
